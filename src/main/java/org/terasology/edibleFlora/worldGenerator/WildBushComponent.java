549ab7b8b90a76a23303f6a4003e6ed7e761ce36
DarkWeird <foxflameless@gmail.com>
2021-07-09 17:23:14 +0300

    Revert "fix(ecs-gestalt): Migrate Components to gestalt's Components."
    This reverts commit 1b1a2d37

--- src/main/java/org/terasology/edibleFlora/worldGenerator/WildBushComponent.java
+++ src/main/java/org/terasology/edibleFlora/worldGenerator/WildBushComponent.java
@@ -2,11 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.edibleFlora.worldGenerator;
 
-import org.terasology.gestalt.entitysystem.component.EmptyComponent;
+import org.terasology.engine.entitySystem.Component;
 
 /**
  * A flag component used to mark prefabs that contain a bush definition.
  * The current stage of the bush definition will then be placed in the world on chunk generation.
  */
-public class WildBushComponent extends EmptyComponent<WildBushComponent> {
+public class WildBushComponent implements Component {
 }
