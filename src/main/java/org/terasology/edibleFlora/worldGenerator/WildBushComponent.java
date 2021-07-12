62f5330662edb88ee1613b8520738f27a94a3a6d
Tobias Nett <skaldarnar@googlemail.com>
2021-07-12 23:23:06 +0200

    chore: use EmptyComponent

--- src/main/java/org/terasology/edibleFlora/worldGenerator/WildBushComponent.java
+++ src/main/java/org/terasology/edibleFlora/worldGenerator/WildBushComponent.java
@@ -1,30 +1,13 @@
-/*
- * Copyright 2017 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.edibleFlora.worldGenerator;
 
 
-import org.terasology.gestalt.entitysystem.component.Component;
+import org.terasology.gestalt.entitysystem.component.EmptyComponent;
 
 /**
  * A flag component used to mark prefabs that contain a bush definition.
  * The current stage of the bush definition will then be placed in the world on chunk generation.
  */
-public class WildBushComponent implements Component<WildBushComponent> {
-    @Override
-    public void copy(WildBushComponent other) {
-
-    }
+public class WildBushComponent extends EmptyComponent<WildBushComponent> {
 }
